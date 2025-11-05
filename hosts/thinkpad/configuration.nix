8f1034833636f466a305330d1cfe4339d3ee6298
Vrashabh Sontakke <vrashabh@sontakke.in>
2025-11-05 18:15:03 +0530

    update flake.lock with latest revisions and hashes; add Slack to home.nix; fix service configuration in legion and thinkpad hosts

--- hosts/thinkpad/configuration.nix
+++ hosts/thinkpad/configuration.nix
@@ -51,10 +51,10 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
   
-  services.logind.lidSwitch = "ignore";
+  services.logind.settings.Login.HandleLidSwitch = "ignore";
 
   # Configure keymap in X11
   services.xserver.xkb = {
