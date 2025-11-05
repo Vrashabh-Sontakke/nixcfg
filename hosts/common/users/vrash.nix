96c1a8cd686b2ece77646dce0a45c5b3ff74c378
Vrashabh Sontakke <vrashabh@sontakke.in>
2025-11-05 21:29:53 +0530

    fix: update package reference in vrash.nix to use stdenv.hostPlatform.system for consistency

--- hosts/common/users/vrash.nix
+++ hosts/common/users/vrash.nix
@@ -25,7 +25,7 @@
     openssh.authorizedKeys.keys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQClfAqcEwFTgnXgQP4YKfewyc4p74CETkI9KXy6Wliwuu4dPfm+456549yOfA3cejy/M20jiFCnUKMONerAXlRTK38NG54RauvjWfk9EZbSuA9hJ3b8TJdTGfvSOQ1fKlq1NtRPD5POhI36fybSRKmDU7lm4fEQc2B27EqwQXd4FCaXW4FaSi/HlessfhG1W9cvDTJ8tkjbP7RdrDOBQD7PQYPYTgmlmkQjrkRbFCguU7Q7ZFmbqcSKZEeD9wmRjHsNSumRtWQk/xxmwZHwbfiIq3w8PHKouk8AGmCh93cnYRS7TlnYAAUL4kD7DY6Q1aPKs++ImjXTjAAN+DRaG9QD vrashabh@sontakke.in"
     ];
-    packages = [inputs.home-manager.packages.${pkgs.system}.default];
+    packages = [inputs.home-manager.packages.${pkgs.stdenv.hostPlatform.system}.default];
   };
   home-manager.users.vrash =
     import ../../../home/vrash/${config.networking.hostName}.nix;
